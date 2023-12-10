5ba9ab854b559489a4d849bdcb56de1a336394a7
Mercedes Mathews <mercedesmathews@Mercedess-MacBook-Air.local>
2023-12-10 18:41:06 -0500

    fixed formatting of compute.c

--- compute.c
+++ compute.c
@@ -3,10 +3,37 @@
 #include "vector.h"
 #include "config.h"
 
-//compute: Updates the positions and locations of the objects in the system based on gravity.
-//Parameters: None
-//Returns: None
-//Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
+__global__ void compute_cuda(vector3** accels){
+	//make an acceleration matrix which is NUMENTITIES squared in size;
+	int k;
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+	int j = blockIdx.y * blockDim.y + threadIdx.y;
+    
+	if (i < NUMENTITIES && j < NUMENTITIES) {
+		//first compute the pairwise accelerations.  Effect is on the first argument.
+		if (i==j) {
+			FILL_VECTOR(accels[i][j],0,0,0);
+		}
+		else {
+			vector3 distance;
+			for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
+			double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+			double magnitude=sqrt(magnitude_sq);
+			double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
+			FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
+		}
+
+		//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+        vector3 accel_sum={0,0,0};
+		for (k=0;k<3;k++)
+			accel_sum[k]+=accels[i][j][k];
+        for (k=0;k<3;k++){
+			hVel[i][k]+=accel_sum[k]*INTERVAL;
+			hPos[i][k]+=hVel[i][k]*INTERVAL;
+		}
+	}
+}
 
 void compute() {
 	vector3* values;
@@ -16,7 +43,7 @@ void compute() {
 	cudaMallocManaged(&values, valuesSize);
     cudaMallocManaged(&accels, accelsSize);
 	for (i=0;i<NUMENTITIES;i++) {
-		accels[i]=&values[i*NUMENTITIES];
+		accels[i]=&values[i*NUMENTITIES]
 	}
 
 	// execute the cuda call
@@ -49,36 +76,6 @@ void compute() {
 	cudaFree(accels);
 }
 
-__global__ void compute_cuda(vector3** accels){
-	//make an acceleration matrix which is NUMENTITIES squared in size;
-	int k;
 
-    int i = blockIdx.x * blockDim.x + threadIdx.x;
-	int j = blockIdx.y * blockDim.y + threadIdx.y;
-    
-	if (i < NUMENTITIES && j < NUMENTITIES) {
-		//first compute the pairwise accelerations.  Effect is on the first argument.
-		if (i==j) {
-			FILL_VECTOR(accels[i][j],0,0,0);
-		}
-		else {
-			vector3 distance;
-			for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
-			double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-			double magnitude=sqrt(magnitude_sq);
-			double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
-			FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-		}
-
-		//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-        vector3 accel_sum={0,0,0};
-		for (k=0;k<3;k++)
-			accel_sum[k]+=accels[i][j][k];
-        for (k=0;k<3;k++){
-			hVel[i][k]+=accel_sum[k]*INTERVAL;
-			hPos[i][k]+=hVel[i][k]*INTERVAL;
-		}
-	}
-}
 
 

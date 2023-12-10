18e0056c173a56aff5d164f1a1f428c88f06236f
Mercedes Mathews <mercedesmathews@Mercedess-MacBook-Air.local>
2023-12-10 18:36:18 -0500

    first pass of converting compute function to cuda

--- compute.c
+++ compute.c
@@ -7,29 +7,27 @@
 //Parameters: None
 //Returns: None
 //Side Effect: Modifies the hPos and hVel arrays with the new positions and accelerations after 1 INTERVAL
-void compute(){
-	//make an acceleration matrix which is NUMENTITIES squared in size;
-	int i,j,k;
-	vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
-	vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
-	for (i=0;i<NUMENTITIES;i++)
+
+void compute() {
+	vector3* values;
+	vector3** accels;
+	size_t valuesSize = sizeof(vector3) * NUMENTITIES * NUMENTITIES;
+	size_t accelsSize = sizeof(vector3*) * NUMENTITIES * NUMENTITIES;
+	cudaMallocManaged(&values, valuesSize);
+    cudaMallocManaged(&accels, accelsSize);
+	for (i=0;i<NUMENTITIES;i++) {
 		accels[i]=&values[i*NUMENTITIES];
-	//first compute the pairwise accelerations.  Effect is on the first argument.
-	for (i=0;i<NUMENTITIES;i++){
-		for (j=0;j<NUMENTITIES;j++){
-			if (i==j) {
-				FILL_VECTOR(accels[i][j],0,0,0);
-			}
-			else{
-				vector3 distance;
-				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
-				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-				double magnitude=sqrt(magnitude_sq);
-				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
-				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-			}
-		}
 	}
+
+	// execute the cuda call
+	dim3 threadPerBlock(4, 4);
+    dim3 numBlocks((NUMENTITIES/threadPerBlock.x) + 1, (NUMENTITIES/threadPerBlock.y) + 1);
+	compute_cuda<<<numBlocks,threadPerBlock>>>(accels);
+
+	// Synchronize CUDA call
+	// Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
+
 	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
 	for (i=0;i<NUMENTITIES;i++){
 		vector3 accel_sum={0,0,0};
@@ -44,6 +42,43 @@ void compute(){
 			hPos[i][k]+=hVel[i][k]*INTERVAL;
 		}
 	}
-	free(accels);
-	free(values);
+
+	// CUDA free accels
+	cudaFree(values);
+	// CUDA free values
+	cudaFree(accels);
 }
+
+__global__ void compute_cuda(vector3** accels){
+	//make an acceleration matrix which is NUMENTITIES squared in size;
+	int k;
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+	int j = blockIdx.y * blockDim.y + threadIdx.y;
+    
+	if (i < NUMENTITIES && j < NUMENTITIES) {
+		//first compute the pairwise accelerations.  Effect is on the first argument.
+		if (i==j) {
+			FILL_VECTOR(accels[i][j],0,0,0);
+		}
+		else {
+			vector3 distance;
+			for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
+			double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+			double magnitude=sqrt(magnitude_sq);
+			double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
+			FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
+		}
+
+		//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+        vector3 accel_sum={0,0,0};
+		for (k=0;k<3;k++)
+			accel_sum[k]+=accels[i][j][k];
+        for (k=0;k<3;k++){
+			hVel[i][k]+=accel_sum[k]*INTERVAL;
+			hPos[i][k]+=hVel[i][k]*INTERVAL;
+		}
+	}
+}
+
+
